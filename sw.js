1dc059894f310d1fbd47a410540f871a6f3bc6ac
Takuro Kitahara <kitaharata@outlook.jp>
2019-08-09 01:46:33 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 workbox.routing.registerRoute(
-  /\.(css|js)$/,
+  /\.(png|css|js)$/,
   new workbox.strategies.NetworkFirst()
 );
